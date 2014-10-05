8f9e424c4b04e479509c7a7182e8b1c2d9b8ac60
cpwitt <cpwitt.dev@gmail.com>
2014-10-05 17:22:32 -0500

    Fixed Formatting and Removed Unnecessary Comment

--- main.c
+++ main.c
@@ -6,10 +6,6 @@
 #pragma config(Motor,  port6,           servoA,    tmotorNormal, openLoop)
 #pragma config(Motor,  port7,           servoB,    tmotorNormal, openLoop)
 
-/* Two methods for controlling speed of the motors. One requires a press to change
- * speed and the \other you hold down continuously (this is the one commented out).
- */
-
 float k1=1; //proportionality constant for relation of joystick and motor speed
 float k2 = 1;
 float currentAngle6 = 0;
@@ -24,39 +20,39 @@ int pause = 4;  // delay between iterations
 float changeSpeedHold()
 {
 	if(vexRT[Btn5D] == 1)
-		{ return 1; }
+	{ return 1; }
 	else
-		{ return 0.2;}
+	{ return 0.2;}
 }
 
 void servoPort6()
 {
 	motor[servoA] = currentAngle6;
-		if(vexRT[Btn5D] == 1 && currentAngle6 > -127 + deltaAngle6)
-		{ //runs when 5D is pressed and the angle won't hit the minimum (it resets to 0 when it does)
-			currentAngle6 -= deltaAngle6;
-			sleep(pause);
-		}
-		if(vexRT[Btn5U] == 1 && currentAngle6 < 127 - deltaAngle6)
-		{ //runs when 5U is pressed and the angle won't hit the maximum
-			currentAngle6 += deltaAngle6;
-			sleep(pause);
-		}
+	if(vexRT[Btn5D] == 1 && currentAngle6 > -127 + deltaAngle6)
+	{ //runs when 5D is pressed and the angle won't hit the minimum (it resets to 0 when it does)
+		currentAngle6 -= deltaAngle6;
+		sleep(pause);
+	}
+	if(vexRT[Btn5U] == 1 && currentAngle6 < 127 - deltaAngle6)
+	{ //runs when 5U is pressed and the angle won't hit the maximum
+		currentAngle6 += deltaAngle6;
+		sleep(pause);
+	}
 }
 
 void servoPort7()
 {
 	motor[servoB] = currentAngle7;
-		if(vexRT[Btn6D] == 1 && currentAngle7 > -127 + deltaAngle7)
-		{ //runs when 6D is pressed and the angle won't hit the minimum (it resets to 0 when it does)
-			currentAngle7 -= deltaAngle7;
-			sleep(pause);
-		}
-		if(vexRT[Btn6U] == 1 && currentAngle7 < 127 - deltaAngle7)
-		{ //runs when 6U is pressed and the angle won't hit the maximum
-			currentAngle7 += deltaAngle7;
-			sleep(pause);
-		}
+	if(vexRT[Btn6D] == 1 && currentAngle7 > -127 + deltaAngle7)
+	{ //runs when 6D is pressed and the angle won't hit the minimum (it resets to 0 when it does)
+		currentAngle7 -= deltaAngle7;
+		sleep(pause);
+	}
+	if(vexRT[Btn6U] == 1 && currentAngle7 < 127 - deltaAngle7)
+	{ //runs when 6U is pressed and the angle won't hit the maximum
+		currentAngle7 += deltaAngle7;
+		sleep(pause);
+	}
 }
 
 task main()
@@ -78,7 +74,7 @@ task main()
 			if (SensorValue(boomMin) == 1) { //if limit "Min" switch is hit, motor turns off
 				boomControlOut = 0;
 			}
-	  }
+		}
 		else if (boomControlIn > 63) { //boom is going up
 			boomControlOut = boomControlIn - 63;
 
