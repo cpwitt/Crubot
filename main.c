8d1d892cb2c2ededa6febd6393be6ece71f51c81
markashworth1 <ashworthma@sasaustin.org>
2014-10-05 22:30:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,40 @@
-#pragma config(Sensor, dgtl1,  boomMax,        sensorTouch)
-#pragma config(Sensor, dgtl2,  boomMin,        sensorTouch)
-#pragma config(Motor,  port2,           leftMotor,     tmotorServoContinuousRotation, openLoop, reversed)
-#pragma config(Motor,  port3,           rightMotor,    tmotorServoContinuousRotation, openLoop, reversed)
-#pragma config(Motor,  port4,           boomMotor,        tmotorServoContinuousRotation, openLoop, reversed)
-#pragma config(Motor,  port6,           servoA,    tmotorNormal, openLoop)
-#pragma config(Motor,  port7,           servoB,    tmotorNormal, openLoop)
+#pragma config(Sensor, dgtl1, boomMax, sensorTouch) //Sensors
+#pragma config(Sensor, dgtl2, boomMin, sensorTouch)
+#pragma config(Motor,  port2, leftMotor, tmotorNomral, openLoop, reversed) //Motors
+#pragma config(Motor,  port3, rightMotor, tmotorNormal, openLoop, reversed)
+#pragma config(Motor,  port4, boomMotor, tmotorNomral, openLoop, reversed)
+#pragma config(Motor,  port6, servoA, tmotorNormal, openLoop) //Servos
+#pragma config(Motor,  port7, servoB, tmotorNormal, openLoop)
 
-float k1=1; //proportionality constant for relation of joystick and motor speed
-float k2 = 1;
+float k1 = 1; //proportionality constant for relation of joystick and motor speed
+float k2 = 0;
+
 float currentAngle6 = 0;
 float currentAngle7 = 0;
-
 float deltaAngle6 = 1; //how much the angle changes each iteration
 float deltaAngle7 = 1;
 int pause = 4;  // delay between iterations
 //ratio of deltaAngle & delay determines how fast the servo turns (50 degs/s right now)
 
 //increases the speed only when Btn5D is held down
-float changeSpeedHold()
+
+void kValues()
 {
 	if(vexRT[Btn5D] == 1)
-	{ return 1; }
+	{ //wheels are slowed down
+		k1 = 0.3;
+		k2 = 0;
+	}
+	if(vexRT[Btn8R] == 1)
+	{ //boom turned on, wheels off
+		k1 = 0;
+		k2 = 1;
+	}
 	else
-	{ return 0.2;}
+	{ //act as normal
+		k1 = 1;
+		k2 = 0;
+	}
 }
 
 void servoPort6()
@@ -59,60 +71,15 @@ task main()
 {
 	while(true)
 	{
-		int boomControlIn = vexRT[Ch1]; //channel one, right joystick, left-right from -127 to 127
-		int boomControlOut = 0;
-		int motorControlInRight = vexRT[Ch2]; //channel two, right joystick, down-up from -127 to 127
-		int motorControlOutRight = 0;
-		int motorControlInLeft = vexRT[Ch3]; //channel three, left joystick, down-up from -127 to 127
-		int motorControlOutLeft = 0;
-
-		// ignore the joystick until it is at least halfway pushed
-		// creates dead space
-		if (boomControlIn < -63) {
-			boomControlOut = boomControlIn + 63;
-
-			if (SensorValue(boomMin) == 1) { //if limit "Min" switch is hit, motor turns off
-				boomControlOut = 0;
-			}
-		}
-		else if (boomControlIn > 63) { //boom is going up
-			boomControlOut = boomControlIn - 63;
-
-			if (SensorValue(boomMax) == 1) { //if limit switch "Max" is hit, motor turns off
-				boomControlOut = 0;
-			}
-		}
-		else {
-			boomControlOut  = 0;
-		}
-		if (motorControlInRight < -63) {
-			motorControlOutRight = motorControlInRight + 63;
-		}
-		else if (motorControlInRight > 63) {
-			motorControlOutRight = motorControlInRight - 63;
-		}
-		else {
-			motorControlOutRight = 0;
-		}
-
-		if (motorControlInLeft < -63) {
-			motorControlOutLeft = motorControlInLeft + 63;
-		}
-		else if (motorControlInLeft > 63) {
-			motorControlOutLeft = motorControlInLeft - 63;
-		}
-		else {
-			motorControlOutLeft = 0;
-		}
-		motor[leftMotor] = motorControlInLeft*k1*2;
-		motor[rightMotor] = motorControlInRight*k1*2;
-		motor[boomMotor] = boomControlOut *k2 * 2; //vexRT[Ch1]*k2; (motor for arm)
+		motor[leftMotor] = vexRT[Ch3]*k1;
+		motor[rightMotor] = vexRT[Ch1]*k1;
+		motor[boomMotor] = vexRT[Ch1]*k2; //vexRT[Ch1]*k2; (motor for arm)
 
 		servoPort6();
 		servoPort7();
-
-		k1 = k2 = changeSpeedHold();
-
+		
+		kValues();
+		if(SensorValue(boomMin) == 1 || SensorValue(boomMax == 1))
+			{k2 = 0;} //if limit "Min" or "Max" switch is hit, motor turns off
 	}
-
 }
